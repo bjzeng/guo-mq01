9ebfb546ab65ab9233c0c7565e1cc43cc12e29bc
bjzeng <zbj520.cn>
2015-09-02 11:44:38 +0800

    试试新增

---
+++ src/test/java/guo/mq/examples/mq01/Test2.java
@@ -0,0 +1,13 @@
+package guo.mq.examples.mq01;
+
+public class Test2 {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
